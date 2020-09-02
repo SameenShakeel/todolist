09bb5b434bb40a6d2fc58b69fd50e0e9826826fd
Sameen Shakeel <sameenshakeel1997@gmail.com>
2020-09-02 11:02:48 -0700

    Added some Components

---
+++ src/AddTodoForm.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+interface Props {
+  addTodo: AddTodo;
+}
+
+interface Props {
+  addTodo: AddTodo;
+}
+
+export const AddTodoForm: React.FC<Props> = ({ addTodo }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          addTodo(text);
+          setText("");
+        }}
+      >
+        Add Todo
+      </button>
+    </form>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { TodoList } from "./TodoList";
+import { AddTodoForm } from "./AddTodoForm";
+
+const initialTodos: Todo[] = [
+  {
+    text: "Walk the dog",
+    complete: false,
+  },
+  {
+    text: "Write app",
+    complete: true,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (text: string) => {
+    const newTodo = { text, complete: false };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
-    </div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </>
   );
 }
 
